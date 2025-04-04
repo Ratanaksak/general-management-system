ca6db58fe3bc47122e9d6f450f656b00ea359149
Ratanaksak <rattanaksak168@gmail.com>
2025-04-04 11:55:48 +0700

    edit

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ MEDIA_URL = '/media/'
 TEMP_DIR = os.path.join(MEDIA_ROOT, 'temp_files')
 os.makedirs(TEMP_DIR, exist_ok=True)
 # Add this to your settings.py
- # Update with your actual path
+POPPLER_PATH = r'C:\Program Files\poppler-24.08.0\Library\bin'  # Update with your actual path
 if os.name == 'nt':  # Windows
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     POPPLER_PATH = r'C:\path\to\poppler-xx\bin'  # Change to your Poppler path
